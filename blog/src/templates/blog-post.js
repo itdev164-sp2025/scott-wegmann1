4aae864ed8feb25f5bd7c99845197fe2a4e1e368
[scott-wegmann-p2] <wegmanns@gmatc.matc.edu>
2025-05-01 12:22:35 -0500

    Completed Activity 9

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,32 +1,42 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
-import { H1} from '../components/Heading'
+import { H1 } from '../components/Heading'
 
 const BlogPost = ({ data }) => {
-    const { title, body } = data.contentfulBlogPost;
+  const { title, body, heroImage } = data.contentfulBlogPost;
 
-    return (
-        <Layout>
-            <H1>{title}</H1>
-            <div dangerouslySetInnerHTML={{_html: body.childMarkdownRemark.html}}></div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <GatsbyImage
+        image={heroImage.gatsbyImageData}
+      />
+      <H1>{title}</H1>
+      <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
+    </Layout>
+  )
 }
 
 export default BlogPost;
 
 export const pageQuery = graphql`
-
-    query blogPostQuery($slug: String!) {
-        contentfulBlogPost(slug: {eq: $slug}) {
-            title
-            slug
-            body {
-                childMarkdownRemark {
-                    html
-                }
-            }
+  query blogPostQuery($slug: String!) {
+    contentfulBlogPost(slug: {eq: $slug}) {
+      title
+      slug
+      body {
+        childMarkdownRemark {
+          html
         }
+      }
+      heroImage {
+        gatsbyImageData (
+          layout: CONSTRAINED
+          placeholder: BLURRED
+          width: 960
+        )
+      }
     }
+  }
 `
