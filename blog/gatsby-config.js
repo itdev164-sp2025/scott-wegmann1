25dfae5d9b2934ab65be7d60a98af392c2c0db31
[scott-wegmann-p2] <wegmanns@gmatc.matc.edu>
2025-05-01 10:01:57 -0500

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -12,7 +12,7 @@ require('dotenv').config({
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `My Blog`,
     description: ``,
     author: `Scott Wegmann `,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
